e1c5a266b42cd10804974c61291a04e8f0736869
Pedro Henrique Silva <pedroupandofacil@hotmail.com>
2023-11-08 22:01:56 -0300

    Adicionando coisas novas

--- src/script.js
+++ src/script.js
@@ -1,6 +1,23 @@
 const btnMobile = document.getElementById("btn-mobile");
 const nav = document.querySelector(".header_nav");
+const btnClose = document.getElementById("btnClose");
+const Modal = document.querySelector(".modal");
+const Cards = document.querySelectorAll(".card");
+
+const fnModal = () => {
+  if (Modal.classList.contains("active")) {
+    Modal.classList.remove("active");
+  } else {
+    Modal.classList.add("active");
+  }
+};
 
 btnMobile.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+Cards.forEach((card) => {
+  card.addEventListener("click", fnModal);
+});
+
+btnClose.addEventListener("click", fnModal);
